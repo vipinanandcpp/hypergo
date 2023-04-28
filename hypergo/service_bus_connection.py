f25d4e7ff20d8f31d0c5ef265c1d0c7b1851a0ec
Matthew Hansen <hypergo@mattian.com>
2023-04-28 15:45:12 -0400

    backward compatible

--- hypergo/service_bus_connection.py
+++ hypergo/service_bus_connection.py
@@ -4,10 +4,10 @@ from hypergo.config import ConfigType
 from hypergo.executor import Executor
 from hypergo.message import MessageType
 from hypergo.storage import Storage
-
+from typing import Union
 
 class ServiceBusConnection(ABC):
-    def general_consume(self, message: MessageType, config: ConfigType, storage: Storage) -> None:
+    def general_consume(self, message: MessageType, config: ConfigType, storage: Union[Storage, None]) -> None:
         executor: Executor = Executor(config, storage)
         for execution in executor.execute(message):
             self.send(execution, config["namespace"])
