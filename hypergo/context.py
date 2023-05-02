bca4d9beff8d02b6b1925916fea21182e33d422e
Matthew Hansen <hypergo@mattian.com>
2023-05-02 14:37:23 -0400

    storage added to context

--- hypergo/context.py
+++ hypergo/context.py
@@ -1,8 +1,18 @@
+import sys
+from typing import Optional
+
 from hypergo.config import ConfigType
 from hypergo.custom_types import TypedDictType
 from hypergo.message import MessageType
+from hypergo.storage import Storage
+
+if sys.version_info >= (3, 11):
+    from typing import NotRequired
+else:
+    from typing_extensions import NotRequired
 
 
 class ContextType(TypedDictType):
     message: MessageType
     config: ConfigType
+    storage: NotRequired[Optional[Storage]]
