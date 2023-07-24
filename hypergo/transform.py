dea2610f1912b3f86201d3206fc2e196e3752136
Matthew Hansen <hypergo@mattian.com>
2023-07-24 01:36:27 -0400

    Implement new features, refactor code, and fix bugs for improved performance and maintainability.

--- hypergo/transform.py
+++ hypergo/transform.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Generator, TypeVar, cast
+from typing import Any, Callable, Generator, List, TypeVar, cast
 
 from hypergo.custom_types import JsonDict, JsonType
 from hypergo.storage import Storage
@@ -10,23 +10,30 @@ T = TypeVar("T")
 class Transform:
     @staticmethod
     def serialization(func: Callable[..., Generator[T, None, None]]) -> Callable[..., Generator[T, None, None]]:
-        return lambda self, data: (Utility.serialize(result) for result in func(self, Utility.deserialize(data)))
+        def serialized_func(self: Any, data: T) -> Generator[T, None, None]:
+            serialized_data = (Utility.serialize(result) for result in func(self, Utility.deserialize(data)))
+            return serialized_data
+
+        return serialized_func
 
     @staticmethod
     def compression(
         key: str,
     ) -> Callable[[Callable[..., Generator[T, None, None]]], Callable[..., Generator[T, None, None]]]:
         def decorator(func: Callable[..., Generator[T, None, None]]) -> Callable[..., Generator[T, None, None]]:
-            def wrapper(self: Any, data: T) -> Generator[T, None, None]:
-                if "compression" in Utility.deep_get(self.config, "input_operations"):
+            def wrapped_func(self: Any, data: T) -> Generator[T, None, None]:
+                input_operations = Utility.deep_get(self.config, "input_operations")
+                output_operations = Utility.deep_get(self.config, "output_operations")
+
+                if "compression" in input_operations:
                     data = Utility.uncompress(data, key)
 
                 for result in func(self, data):
-                    if "compression" in Utility.deep_get(self.config, "output_operations"):
+                    if "compression" in output_operations:
                         result = Utility.compress(result, key)
                     yield result
 
-            return wrapper
+            return wrapped_func
 
         return decorator
 
@@ -34,19 +41,23 @@ class Transform:
     def pass_by_reference(
         func: Callable[..., Generator[T, None, None]]
     ) -> Callable[..., Generator[JsonDict, None, None]]:
-        def wrapper(self: Any, data: T) -> Generator[JsonDict, None, None]:
+        def wrapped_func(self: Any, data: T) -> Generator[JsonDict, None, None]:
             storage: Storage = self.storage.use_sub_path("passbyreference")
             the_data: JsonType = cast(JsonType, data)
-            if "pass_by_reference" in Utility.deep_get(self.config, "input_operations"):
-                storagekey: str = Utility.deep_get(cast(JsonDict, data), "storagekey")
-                the_data = Utility.objectify(storage.load(storagekey))
-            for result in func(self, the_data):
-                outmessage: JsonDict = cast(JsonDict, result)
-                if "pass_by_reference" in Utility.deep_get(self.config, "output_operations"):
-                    str_result = Utility.stringify(outmessage)
-                    out_storagekey = Utility.hash(str_result)
-                    storage.save(out_storagekey, str_result)
-                    outmessage = {"storagekey": out_storagekey}
-                yield outmessage
+            input_operations: List[str] = Utility.deep_get(self.config, "input_operations")
+            output_operations: List[str] = Utility.deep_get(self.config, "output_operations")
 
-        return wrapper
+            if "pass_by_reference" in input_operations:
+                storage_key = Utility.deep_get(cast(JsonDict, data), "storagekey")
+                the_data = Utility.objectify(storage.load(storage_key))
+
+            for result in func(self, the_data):
+                out_message = cast(JsonDict, result)
+                if "pass_by_reference" in output_operations:
+                    str_result = Utility.stringify(out_message)
+                    out_storage_key = Utility.hash(str_result)
+                    storage.save(out_storage_key, str_result)
+                    out_message = {"storagekey": out_storage_key}
+                yield out_message
+
+        return wrapped_func
