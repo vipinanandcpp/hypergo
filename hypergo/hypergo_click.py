00cdbc2f472603211cbe69192a1e221cec91564a
Vipin Anand <VAnand@linklogistics.com>
2023-11-10 17:20:16 -0500

    hypergo-logger

--- hypergo/hypergo_click.py
+++ hypergo/hypergo_click.py
@@ -15,13 +15,13 @@ def main() -> int:
     return 0
 
 
-# @main.command()
+@main.command()
 def shell() -> int:
     HypergoCmd(HYPERGO_CLI).cmdloop()
     return 0
 
 
-# @main.command()
+@main.command()
 @click.argument('ref', type=click.STRING)
 @click.argument('arg', nargs=-1)
 @click.option('--stdin', is_flag=True, default=False, help='Read input from stdin.')
@@ -32,7 +32,7 @@ def stdio(ref: str, arg: Tuple[str, ...], stdin: bool) -> int:
     return HYPERGO_CLI.stdio(ref, *list(arg))
 
 
-# @main.command()
+@main.command()
 @click.argument('ref', type=click.STRING)
 @click.argument('arg', nargs=-1)
 @click.option(
--- hypergo/secrets.py
+++ hypergo/secrets.py
@@ -4,7 +4,6 @@ from typing import Any
 
 
 class Secrets(ABC):
-
     @classmethod
     @abstractmethod
     def get(cls, key: str) -> Any:
