9f02ee1228d538e42fb505ce2655ae6329551163
Vipin Anand <VAnand@linklogistics.com>
2023-11-16 19:32:38 -0500

    hypergo-logger

--- tests/test_dynamic_input_bindings.py
+++ tests/test_dynamic_input_bindings.py
@@ -2,8 +2,6 @@ import glom
 import os
 import sys
 import unittest
-from unittest.mock import patch
-import requests
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -22,10 +20,7 @@ class TestDynamicRoutingKey(unittest.TestCase):
         }
         return super().setUp()
 
-    @patch("hypergo.monitors.requests.post")
-    @patch.dict(os.environ, {"LOG_ANALYTICS_WORKSPACE_ID": "shmorkspace_id"})
-    @patch.dict(os.environ, {"LOG_ANALYTICS_PRIMARY_KEY": "shmimary_key"})
-    def test_dynamic_routing_key(self, mock_post):
+    def test_dynamic_routing_key(self):
         cfg: ConfigType = {
                             "namespace": "datalink",
                             "name": "snowflakedbexecutor",
@@ -38,15 +33,13 @@ class TestDynamicRoutingKey(unittest.TestCase):
                             "custom_properties": {"specleasing.select_query":
                                             "SELECT * FROM DP_LNK_DEV_DB.DP_LNK_ENTITIES.VW_UNIT_VALUATIONS LIMIT 10;"}
                         }
-        response = requests.Response()
-        response.status_code = 200
-        mock_post.return_value = response
         executor = Executor(cfg)
         context: ContextType = {"message": self.message, "config": cfg}
         try:
             executor.get_args(context=context)
         except glom.PathAccessError as e:
             raise e
+
 
 if __name__ == '__main__':
     unittest.main()
