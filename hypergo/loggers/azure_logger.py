6f3e046c647456a4035e80676f65bd4a1a791d66
Vipin Anand <vipin.anand.cpp@gmail.com>
2024-01-19 21:27:40 -0500

    hypergo-logger

--- hypergo/loggers/azure_logger.py
+++ hypergo/loggers/azure_logger.py
@@ -1,26 +1,16 @@
 import logging
 from typing import Optional, Union
+from azure.monitor.opentelemetry import configure_azure_monitor
 from opentelemetry import trace
-from opentelemetry._logs import set_logger_provider
-from opentelemetry.sdk._logs import (LoggerProvider, LoggingHandler)
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.sdk.trace import TracerProvider
-from azure.monitor.opentelemetry.exporter import AzureMonitorLogExporter
-
 from hypergo.loggers.base_logger import BaseLogger
 from hypergo.secrets import Secrets
 
-trace.set_tracer_provider(TracerProvider())
-# Attach LoggingHandler to root logger
-logging.getLogger().addHandler(LoggingHandler())
-logging.getLogger().setLevel(logging.NOTSET)
+
+# Ensure that configure_azure_monitor is called only once
+configure_azure_monitor_called: bool = False
 
 
 class AzureLogger(BaseLogger):
-    exporter: AzureMonitorLogExporter = None
-    logger_provider: LoggerProvider = LoggerProvider()
-    set_logger_provider(logger_provider)
-
     def __init__(
         self,
         secrets: Secrets,
@@ -29,16 +19,18 @@ class AzureLogger(BaseLogger):
         log_format: Optional[Union[str, logging.Formatter]] = None,
     ) -> None:
         super().__init__(name=name, log_level=log_level, log_format=log_format)
-        if not AzureLogger.exporter:
-            AzureLogger.exporter = AzureMonitorLogExporter(
-                connection_string=secrets.get("APPLICATIONINSIGHTS_CONNECTION_STRING"), disable_offline_storage=True)
-            AzureLogger.logger_provider.add_log_record_processor(BatchLogRecordProcessor(AzureLogger.exporter))
+        global configure_azure_monitor_called
+        if not configure_azure_monitor_called:
+            configure_azure_monitor(connection_string=secrets.get("APPLICATIONINSIGHTS-CONNECTION-STRING"),
+                                    disable_offline_storage=True)
+            configure_azure_monitor_called = True
 
     def log(self, message: str, level: Optional[int] = None) -> None:
         if level is None:
             level = self.log_level
+        handler: logging.Handler = self.get_handler()
         logger = logging.getLogger(self.name)
-        logger.addHandler(self.get_handler())
+        logger.addHandler(handler)
         logger.setLevel(level)
         # Get a tracer for the current module.
         with trace.get_tracer(__name__).start_as_current_span(self.name):
