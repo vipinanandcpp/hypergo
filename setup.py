8c17cbee582c90079bdf4614e605af8896f6a11f
Vipin Anand <vipin.anand.cpp@gmail.com>
2024-02-22 22:23:34 -0500

    hypergo-metrics

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 # Define the setup parameters
 setup(
     name='hypergo',
-    version='0.3.5',  # Enclose the version number in quotes
+    version='0.3.6',  # Enclose the version number in quotes
     description='Project for service bus',
     long_description_content_type='text/markdown',
     packages=find_packages(),
